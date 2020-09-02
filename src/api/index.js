85698fe1177f8d61c3c738e94fdd68ad9367993f
abdulrahmanabdullah <nfs056@gmail.com>
2020-09-02 07:06:57 +0300

    add backend url on api folder

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:1234';
+const baseURL = 'https://abdulrahmanblog.herokuapp.com/';
 export const getArticles = async () => {
   try {
     const response = await axios.get(`${baseURL}/blogs`);
